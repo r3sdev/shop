32a7596a597da30d3b2fd587dec6578118b37ddb
Ramsy de Vos <ramsy@ramsy.it>
2020-06-01 10:54:33 +0200

    Add logging for verifyTwoFactorAuthenticationCode

--- auth/src/2fa/verify-2fa-code.ts
+++ auth/src/2fa/verify-2fa-code.ts
@@ -1,13 +1,17 @@
-import speakeasy from 'speakeasy';
-import type { UserDoc } from '../models/user';
+import speakeasy, { TotpVerifyOptions } from 'speakeasy';
 
 export function verifyTwoFactorAuthenticationCode(
-         twoFactorAuthenticationCode: string,
-         user: Express.Request['currentUser'],
-       ) {
-         return speakeasy.totp.verify({
-           secret: user!.twoFactorAuthCode!,
-           encoding: 'base32',
-           token: twoFactorAuthenticationCode,
-         });
-       }
+  twoFactorAuthenticationCode: string,
+  user: Express.Request['currentUser'],
+) {
+
+  const data: TotpVerifyOptions = {
+    secret: user!.twoFactorAuthCode!,
+    encoding: 'base32',
+    token: twoFactorAuthenticationCode,
+  };
+
+  console.log('verifyTwoFactorAuthenticationCode', data);
+  
+  return speakeasy.totp.verify(data);
+}
