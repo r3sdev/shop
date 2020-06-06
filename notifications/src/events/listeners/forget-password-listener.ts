293214df62a012dbc6a502d5f1263218c082961a
Ramsy de Vos <ramsy@ramsy.it>
2020-06-06 08:02:08 +0200

    Rename notification -> notifications

--- notifications/src/events/listeners/forget-password-listener.ts
+++ notifications/src/events/listeners/forget-password-listener.ts
@@ -1,13 +1,24 @@
 import { Message } from 'node-nats-streaming';
 import { UserForgotPasswordEvent, Listener, Subjects } from '@ramsy-it/common';
 import { queueGroupName } from './queue-group-name';
+import sendEmail from '../../services/send-email';
+import forgotPassword from '../../email-templates/user/forgot-password';
 
 export class ForgetPasswordListener extends Listener<UserForgotPasswordEvent> {
   subject: Subjects.UserForgotPassword = Subjects.UserForgotPassword;
   queueGroupName = queueGroupName;
 
   async onMessage(data: UserForgotPasswordEvent['data'], msg: Message) {
-    console.log(event);
+
+    const {html, text} = forgotPassword(data.link);
+
+    await sendEmail({
+      from: 'support@ramsy.dev',
+      to: data.email,
+      html,
+      text,
+      subject: 'Reset your password'
+    })
 
     msg.ack();
   }
