8abdb1372039ac1d0649a8c06c5065853a7138ba
Ramsy de Vos <ramsy@ramsy.it>
2020-06-10 18:28:07 +0200

    Add styling to new product

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -7,10 +7,10 @@ const NewTicket = () => {
   const [title, setTitle] = React.useState('');
   const [price, setPrice] = React.useState('');
 
-  const {doRequest, errors} = useRequest({
+  const { doRequest, errors } = useRequest({
     url: '/api/tickets',
     method: 'post',
-    body: {title, price},
+    body: { title, price },
     onSuccess: () => Router.push('/')
   });
 
@@ -31,29 +31,37 @@ const NewTicket = () => {
 
 
   return (
-    <div>
-      <h1>Create a new ticket</h1>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Title</label>
-          <input
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            className="form-control"
-          />
+    <div className="offset-3 col-6">
+      <div class="card">
+        <div class="card-body">
+
+          <div>
+            <h1>Create a new product</h1>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <label>Title</label>
+                <input
+                  value={title}
+                  onChange={e => setTitle(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label>Price</label>
+                <input
+                  value={price}
+                  onBlur={onBlur}
+                  onChange={e => setPrice(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+              {errors}
+              <button className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+
         </div>
-        <div className="form-group">
-          <label>Price</label>
-          <input
-            value={price}
-            onBlur={onBlur}
-            onChange={e => setPrice(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        {errors}
-        <button className="btn btn-primary">Submit</button>
-      </form>
+      </div>
     </div>
   )
 };
