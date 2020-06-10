d365fb28c0ce0a5cd8f07b5688ef461dee4630c5
Ramsy de Vos <ramsy@ramsy.it>
2020-06-10 18:05:22 +0200

    Redirect to signed out page after signing out

--- client/pages/auth/signout.js
+++ client/pages/auth/signout.js
@@ -7,7 +7,7 @@ export default () => {
     url: '/api/users/signout',
     method: 'post',
     body: {},
-    onSuccess: () => Router.push('/')
+    onSuccess: () => Router.push('/auth/signed-out')
   })
 
   React.useEffect(() => {
