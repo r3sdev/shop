d79c5876999a3d2d5387e8c4c524eab3100f26c5
Ramsy de Vos <ramsy@ramsy.it>
2020-06-18 06:00:56 +0200

    Fix spelling error

--- client/pages/auth/email/confirm.js
+++ client/pages/auth/email/confirm.js
@@ -2,7 +2,7 @@
 export default () => {
   return (
     <div>
-      <h3>Please confirm your email addresss</h3>
+      <h3>Please confirm your email address</h3>
       <p>
         Check your inbox and click on the link to confirm your email address
         and login
