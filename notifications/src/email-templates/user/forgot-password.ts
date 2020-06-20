e772a91b48c4ddb98c121286ca31db7ba46304e9
Ramsy de Vos <ramsy@ramsy.it>
2020-06-20 20:34:52 +0200

    Update notifications

--- notifications/src/email-templates/user/forgot-password.ts
+++ notifications/src/email-templates/user/forgot-password.ts
@@ -6,7 +6,7 @@ Please create a new password by clicking this link: ${link}
 This request will expire in 1 hour.
 
 If you did not request this change, no changes have been made to your account. 
-We recommend that you review your security settings: https://ticketing.ramsy.dev/docs/accounts/security/
+We recommend that you review your security settings: https://shop.ramsy.dev/docs/accounts/security/
 `,
   html: `
 We received a request to reset your password.<br />
@@ -16,7 +16,7 @@ This request will expire in 1 hour.<br />
 <br />
 If you did not request this change, no changes have been made to your account. <br />
 We recommend that you review your security settings: 
-<a href="https://ticketing.ramsy.dev/docs/accounts/security/">https://ticketing.ramsy.dev/docs/accounts/security/</a> <br />
+<a href="https://shop.ramsy.dev/docs/accounts/security/">https://shop.ramsy.dev/docs/accounts/security/</a> <br />
 
       `,
 });
