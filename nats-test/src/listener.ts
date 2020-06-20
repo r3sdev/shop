5f31f9a2aeec8f44ec72d0d92a255631928926f2
Ramsy de Vos <ramsy@ramsy.it>
2020-06-20 20:34:45 +0200

    Update nats-test

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -4,7 +4,7 @@ import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
-const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+const stan = nats.connect('shop', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
