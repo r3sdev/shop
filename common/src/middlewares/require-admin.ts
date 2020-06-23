ca3536b1a592b6942fe2489ed729d68004c5fe0d
Ramsy de Vos <ramsy@ramsy.it>
2020-06-23 09:33:55 +0200

    Updates

--- common/src/middlewares/require-admin.ts
+++ common/src/middlewares/require-admin.ts
@@ -1,21 +1,31 @@
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
-export const requireAdmin = async (req: Request, res: Response, next: NextFunction) => {
+export const requireAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const client = axios.create({
+    baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+    headers: req.headers,
+  });
 
-    if (req.currentUser) {
-        const user = await axios.get('/api/users/currentuser', {withCredentials: true});
-        console.log('requireAdmin', user);
-    } else {
-        console.log('requireAdmin', 'No user found');
-    }
-    
-    console.debug('requireAdmin', req.currentUser)
-    
-    if (!req.currentUser?.isAdmin) {
-        throw new NotAuthorizedError()
-    }
+  if (req.currentUser) {
+    const user = await client.get('/api/users/currentuser', {
+      withCredentials: true,
+    });
+    console.log('requireAdmin', user);
+  } else {
+    console.log('requireAdmin', 'No user found');
+  }
 
-    next();
-}
+  console.debug('requireAdmin', req.currentUser);
+
+  if (!req.currentUser?.isAdmin) {
+    throw new NotAuthorizedError();
+  }
+
+  next();
+};
