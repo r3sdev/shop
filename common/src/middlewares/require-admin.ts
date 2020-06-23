60bb2cdbc9391111576de5b21be8794741f91ef4
Ramsy de Vos <ramsy@ramsy.it>
2020-06-23 09:13:30 +0200

    Updates

--- common/src/middlewares/require-admin.ts
+++ common/src/middlewares/require-admin.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction} from 'express';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
 export const requireAdmin= (req: Request, res: Response, next: NextFunction) => {
+
+    console.debug('requireAdmin', req.currentUser)
+    
     if (!req.currentUser?.isAdmin) {
         throw new NotAuthorizedError()
     }
