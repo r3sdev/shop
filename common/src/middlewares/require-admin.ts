d7fe6fe49d4711332a497267f6c587a366227179
Ramsy de Vos <ramsy@ramsy.it>
2020-06-23 10:30:41 +0200

    Updates

--- common/src/middlewares/require-admin.ts
+++ common/src/middlewares/require-admin.ts
@@ -1,31 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
-import axios from 'axios';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
-export async function requireAdmin(
+export const requireAdmin = (
   req: Request,
   res: Response,
   next: NextFunction,
-) {
-  if (!req.currentUser) {
-    throw new NotAuthorizedError();
-  }
+) => {
 
-  const client = axios.create({
-    baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-    headers: req.headers,
-  });
-
-  const { data: user } = await client.get<{ isAdmin?: boolean }>(
-    '/api/users/currentuser',
-    { withCredentials: true },
-  );
-
-  console.log('requireAdmin', user);
-  
-  if (!user || !user.isAdmin) {
+  console.log('requireAdmin', req.currentUser)
+  if (!req.currentUser?.isAdmin) {
     throw new NotAuthorizedError();
   }
 
   next();
-}
+};
