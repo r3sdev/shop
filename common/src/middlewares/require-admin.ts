94ab06108bba56cf18573f6229a79c898f0d0d57
Ramsy de Vos <ramsy@ramsy.it>
2020-06-23 09:25:10 +0200

    Updates

--- common/src/middlewares/require-admin.ts
+++ common/src/middlewares/require-admin.ts
@@ -1,8 +1,16 @@
 import { Request, Response, NextFunction} from 'express';
+import axios from 'axios';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
-export const requireAdmin= (req: Request, res: Response, next: NextFunction) => {
+export const requireAdmin = async (req: Request, res: Response, next: NextFunction) => {
 
+    if (req.currentUser) {
+        const user = await axios.get('/api/users/currentuser', {withCredentials: true});
+        console.log('requireAdmin', user);
+    } else {
+        console.log('requireAdmin', 'No user found');
+    }
+    
     console.debug('requireAdmin', req.currentUser)
     
     if (!req.currentUser?.isAdmin) {
