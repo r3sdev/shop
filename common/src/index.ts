b2596f5f7bbc00b2925dbf577a39b26295c19b84
Ramsy de Vos <ramsy@ramsy.it>
2020-06-23 15:29:46 +0200

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -8,7 +8,6 @@ export * from './errors/request-validation-error';
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
-export * from './middlewares/require-admin';
 export * from './middlewares/validate-request';
 
 export * from './events/base-listener';
--- common/src/middlewares/require-auth.ts
+++ common/src/middlewares/require-auth.ts
@@ -3,6 +3,7 @@ import { NotAuthorizedError } from '../errors/not-authorized-error';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
     if (!req.currentUser) {
+        console.log('UnAuthorized', req.currentUser)
         throw new NotAuthorizedError()
     }
 
