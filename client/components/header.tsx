5adb8ccbed6c61381253e5b056fcd22d304a9e03
Ramsy de Vos <ramsy@ramsy.it>
2020-06-24 13:01:59 +0200

    Update header

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { ButtonToolbar, DropdownButton, ButtonGroup, Dropdown } from 'react-bootstrap';
+import Router from 'next/router';
 
 export default ({ currentUser }) => {
 
+  const isAdmin = currentUser?.isAdmin
+
+  const pathname = Router.router?.pathname || ''
+
   const links = [
-    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
-    !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'Sell Products', href: '/products/new' },
-    currentUser && { label: 'My Orders', href: '/orders' },
-    currentUser && { label: 'Profile', href: '/profile' },
+    currentUser && { label: 'Products', href: '/products' },
     currentUser?.isAdmin && { label: 'Categories', href: '/categories' },
-    currentUser?.isAdmin && { label: 'Admin', href: '/admin' },
-    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+    currentUser && { label: 'Orders', href: '/orders' },
   ]
     .filter(linkConfig => linkConfig)
     .map(({ label, href }) => {
@@ -24,7 +25,18 @@ export default ({ currentUser }) => {
       </li>
     })
 
-  return <nav className="navbar navbar-light bg-light">
+  const onSelect = (eventKey: string) => {
+    switch (eventKey) {
+      case 'profile':
+        return Router.push('/profile');
+      case 'admin':
+        return Router.push('/admin');
+      case 'signout':
+        return Router.push('/auth/signout');
+    }
+  }
+
+  return <nav className="navbar sticky-top navbar-light bg-light">
     <Link href="/">
       <a className="navbar-brand">
         <FontAwesomeIcon
@@ -38,6 +50,54 @@ export default ({ currentUser }) => {
     <div className="d-flex justify-content-end">
       <ul className="nav d-flex align-items-center">
         {links}
+        {
+          !currentUser
+            ? (
+              <ButtonToolbar>
+                <Link href={'/auth/signin'}>
+                  <button type="button" className="btn btn-link">Sign in</button>
+                </Link>
+                <Link href={'/auth/signup'}>
+                  <button type="button" className="btn btn-primary">Sign up</button>
+                </Link>
+              </ButtonToolbar>
+            )
+            : (
+              <DropdownButton
+                onSelect={onSelect}
+                as={ButtonGroup}
+                id="auth-dropdown"
+                variant="primary"
+                title={
+                  <>
+                    <FontAwesomeIcon icon={faUser} className="mr-2" />
+                    {currentUser.fullName}
+                  </>
+                }
+              >
+                <Dropdown.Item eventKey="profile"
+                  active={pathname.includes('profile')}
+                >
+                  Profile
+                  </Dropdown.Item>
+                {
+                  isAdmin && (
+                    <>
+                      <Dropdown.Item eventKey="admin"
+                        active={pathname.includes('admin')}
+                      >
+                        Admin
+                      </Dropdown.Item>
+                      <Dropdown.Divider />
+                    </>
+                  )
+                }
+                <Dropdown.Item eventKey="signout">
+                  Sign out
+                </Dropdown.Item>
+              </DropdownButton>
+            )
+        }
       </ul>
     </div>
   </nav>
