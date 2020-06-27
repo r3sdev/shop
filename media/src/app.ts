035ce087849e0b3f916d00afa03f351d000bb172
Ramsy de Vos <ramsy@ramsy.it>
2020-06-27 09:54:44 +0200

    Create media service

---
+++ media/src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import helmet from 'helmet';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@ramsy-dev/microservices-shop-common';
+
+import { uploadRouter } from './routes/upload';
+
+const app = express();
+
+app.use(helmet());
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    name: 'shop',
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+
+app.use(currentUser);
+
+
+app.use(uploadRouter);
+
+// Catch all non defined urls
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
