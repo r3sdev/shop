4f69175a1adc2fd4450f3123c529b76d9f8b9422
Ramsy de Vos <ramsy@ramsy.it>
2020-06-28 15:34:20 +0200

    Updates

--- common/src/events/product-updated-event.ts
+++ common/src/events/product-updated-event.ts
@@ -8,10 +8,11 @@ export interface ProductUpdatedEvent {
     title: string;
     price: number;
     cost: number;
+    imageUrl: string;
     category?: {
       id: string;
       title: string;
-    }
+    };
     userId: string;
     orderId?: string;
   };
