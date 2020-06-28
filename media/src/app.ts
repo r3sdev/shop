3f8d323a058ad2961cf8eb716ae71513c2fb030c
Ramsy de Vos <ramsy@ramsy.it>
2020-06-28 15:41:55 +0200

    Update media

--- media/src/app.ts
+++ media/src/app.ts
@@ -1,22 +1,47 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
 import helmet from 'helmet';
 import 'express-async-errors';
-// import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import {
   errorHandler,
   NotFoundError,
   currentUser,
 } from '@ramsy-dev/microservices-shop-common';
-import upload from 'express-fileupload';
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
 
-import { uploadRouter } from './routes/upload';
+const FOLDER_NAME = 'images';
+const CDN_URL ='https://cdn-ramsy-dev.ams3.cdn.digitaloceanspaces.com/images/';
+
+var s3 = new aws.S3({
+  endpoint: 'ams3.digitaloceanspaces.com',
+  accessKeyId: 'CA4KOUCR3KBJX7PD6EWE', // process.env.SPACES_KEY,
+  secretAccessKey: 'rgLdulL4QuvmnFhKyyuC3hT793x5kj4+ieaIOtyAAGw', // process.env.SPACES_SECRET,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'cdn-ramsy-dev',
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, {
+        originalname: file.originalname,
+      });
+    },
+    key: function (request, file, cb) {
+      console.log(file);
+      cb(null, `${FOLDER_NAME}/${file.originalname}`);
+    },
+  }),
+});
 
 const app = express();
 
 app.use(helmet());
 app.set('trust proxy', true);
-// app.use(json());
 app.use(
   cookieSession({
     name: 'shop',
@@ -24,12 +49,14 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   }),
 );
-app.use(upload);
 
 app.use(currentUser);
 
 
-app.use(uploadRouter);
+app.post('/api/media/upload', upload.single('image'), function (req, res, next) {
+  res.send({ image: CDN_URL + req.file.originalname });
+});
+
 
 // Catch all non defined urls
 app.all('*', () => {
--- media/src/routes/upload.ts
+++ media/src/routes/upload.ts
@@ -1,17 +1,36 @@
 import express, { Request, Response, NextFunction } from 'express';
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+
 import { requireAuth } from '@ramsy-dev/microservices-shop-common';
+
+var s3 = new aws.S3({
+  endpoint: 'ams3.digitaloceanspaces.com',
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'cdn-ramsy-dev',
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
 
 const router = express.Router();
 
 router.post(
-  '/api/media/upload',
-  // (req: Request, res: Response, next: NextFunction) =>
-  //   requireAuth(req, res, next, {withAdmin: true}),
-  async (req: Request, res: Response) => {
-    console.log(req.files)
-    
-    res.send(req.body);
-  },
-);
+  '/api/media/upload', 
+  upload.array('image', 1),
+  function (req: Request, res: Response, next: NextFunction) {
+    res.send('Successfully uploaded ' + req.files.length + ' files!');
+
+  });
 
 export { router as uploadRouter };
