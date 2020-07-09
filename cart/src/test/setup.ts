19ebf1e946364fb93272e54e9fb1a5d366d4bbb1
Ramsy de Vos <ramsy@ramsy.it>
2020-07-09 17:51:43 +0200

    Add test

---
+++ cart/src/test/setup.ts
@@ -0,0 +1,72 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+interface SigninOptions {
+  isAdmin?: boolean
+}
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin: (options?: SigninOptions) => string[];
+    }
+  }
+}
+
+global.signin = (options = {}) => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+    isAdmin: !!options?.isAdmin,
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: jwt_data }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessonJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessonJSON).toString('base64');
+
+  // return a string thats the cookie with the encode data
+
+  return [`shop=${base64}`];
+};
+
+jest.mock('../nats-wrapper');
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  // Setup ENV variables
+  process.env.JWT_KEY = 'test';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
