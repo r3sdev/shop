915619dd05e0ca2f4928042af5a1312e15a733d1
Ramsy de Vos <ramsy@ramsy.it>
2020-07-19 08:54:33 +0200

    Fix cart product

--- cart/src/models/cart.ts
+++ cart/src/models/cart.ts
@@ -40,19 +40,15 @@ var productSchema = new mongoose.Schema(
   {
     id: {
       type: String,
-      required: true,
     },
     title: {
       type: String,
-      required: true,
     },
     price: {
       type: Number,
-      required: true,
     },
     imageUrl: {
       type: String,
-      required: true,
     },
   },
   {
