365c91e359b1029210a0de35740fc8bc55b60347
Ramsy de Vos <ramsy@ramsy.it>
2020-07-25 11:08:30 +0200

    Add new route

--- logs/src/app.ts
+++ logs/src/app.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import helmet from 'helmet';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
 import {
   errorHandler,
   NotFoundError,
 } from '@ramsy-dev/microservices-shop-common';
+import { createLogRouter } from './routes/new';
 
 const { healthz } = require('express-healthz');
 
@@ -16,11 +16,13 @@ app.set('trust proxy', true);
 
 app.use(json());
 
-app.use(healthz)
+app.use(healthz);
+
+app.use(createLogRouter);
 
 // Catch all non defined urls
 app.all('*', (req) => {
-  console.log(req.url)
+  console.log(req.url);
   throw new NotFoundError();
 });
 
--- logs/src/routes/new.ts
+++ logs/src/routes/new.ts
@@ -3,18 +3,13 @@ import { body } from 'express-validator';
 import {
   validateRequest,
 } from '@ramsy-dev/microservices-shop-common';
-import mongoose from 'mongoose';
 
 const router = express.Router();
 
 router.post(
-  '/api/products',
+  '/api/logs',
   [
     body('title').not().isEmpty().withMessage('Title is required'),
-    body('price')
-      .isFloat({ gt: 0 })
-      .withMessage('Price must be greater than 0'),
-    body('cost').isFloat({ gt: 0 }).withMessage('Cost must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -22,4 +17,4 @@ router.post(
   },
 );
 
-export { router as createProductRouter };
+export { router as createLogRouter };
