71ba8fe4a4ee5c92a08faeacb2e9b821f5c04014
Ramsy de Vos <ramsy@ramsy.it>
2020-07-26 10:33:08 +0200

    Create custom server

---
+++ client/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const _next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const next = _next({ dev })
+const handle = next.getRequestHandler()
+
+const { healthz } = require('express-healthz');
+
+const app = express()
+app.use(healthz);
+
+
+next.prepare().then(() => {
+
+    app.all('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    app.listen(port, (err) => {
+        if (err) throw err
+        console.log(`Client ready on http://localhost:${port}`)
+    })
+})
