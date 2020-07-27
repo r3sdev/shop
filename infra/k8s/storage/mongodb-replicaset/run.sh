15bee6f3d074ec9dff098fc5eb01858116efe044
Ramsy de Vos <ramsy@ramsy.it>
2020-07-27 12:42:43 +0200

    Use env variable for namespace

--- infra/k8s/storage/mongodb-replicaset/run.sh
+++ infra/k8s/storage/mongodb-replicaset/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-helm install shop-mongodb-replicaset stable/mongodb-replicaset --namespace development -f values.yaml
+helm install shop-mongodb-replicaset stable/mongodb-replicaset --namespace $NAMESPACE -f values.yaml
