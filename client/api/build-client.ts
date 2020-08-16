4aa6decd9b1c13d1f1bf659b5b1ff088732d3070
Ramsy de Vos <ramsy@ramsy.it>
2020-08-16 17:18:07 +0200

    Update build-client

--- client/api/build-client.ts
+++ client/api/build-client.ts
@@ -19,36 +19,9 @@ export const buildClient = ({ req }) => {
     client.interceptors.response.use(
       res => res,
       error => {
-        if (error.response.status === 404) {
-          throw new Error(`404: ${error.config.url} not found`);
+        if (error.response.status) {
+          throw new Error(`${error.response.status}: ${process.env.BASE_URL}${error.config.url} not found`);
         }
-        if (error.response.status === 502) {
-          throw new Error(`502: ${error.config.url} not ready`);
-        }
-        if (error.response.status === 503) {
-          throw new Error(`503: ${error.config.url} not ready`);
-        }
-        // // Error 😨
-        // if (error.response) {
-        //   /*
-        //    * The request was made and the server responded with a
-        //    * status code that falls out of the range of 2xx
-        //    */
-        //   console.log(error.response.data);
-        //   console.log(error.response.status);
-        //   console.log(error.response.headers);
-        // } else if (error.request) {
-        //   /*
-        //    * The request was made but no response was received, `error.request`
-        //    * is an instance of XMLHttpRequest in the browser and an instance
-        //    * of http.ClientRequest in Node.js
-        //    */
-        //   console.log(error.request);
-        // } else {
-        //   // Something happened in setting up the request and triggered an Error
-        //   console.log('Error', error.message);
-        // }
-        // console.log(error);
   
         throw error;
       }
@@ -63,11 +36,6 @@ export const buildClient = ({ req }) => {
      * Requests should be made with a baseUrl of ''
      */
 
-    const client = axios.create();
-
-
-
-
-    return client;
+    return axios.create();
   }
 }
