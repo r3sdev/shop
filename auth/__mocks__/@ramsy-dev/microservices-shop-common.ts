95bfc91752d0698476f46326d18b81c88d5affd1
Ramsy de Vos <ramsy@ramsy.it>
2020-08-23 17:19:49 +0200

    Add common module mock

--- auth/__mocks__/@ramsy-dev/microservices-shop-common.ts
+++ auth/__mocks__/@ramsy-dev/microservices-shop-common.ts
@@ -1,2 +1,19 @@
-export default jest.fn();
-export const currentUser = jest.fn();
+import { NextFunction } from "express";
+
+
+export const Publisher = jest.fn();
+
+export const currentUser = jest.requireActual('@ramsy-dev/microservices-shop-common').currentUser;
+
+export const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+    return next();
+}
+export const errorHandler = (req: Request, res: Response, next: NextFunction) => {
+    return next();
+}
+
+//  () => (req: Request, res: Response, next: NextFunction) => {
+//     // req.user = {};
+//     return next();
+//   });
+
