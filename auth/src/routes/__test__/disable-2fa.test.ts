25546e99795614aea65b994fda4c422e56880f13
Ramsy de Vos <ramsy@ramsy.it>
2020-09-07 16:00:51 +0200

    Add disable-2fa test

--- auth/src/routes/__test__/disable-2fa.test.ts
+++ auth/src/routes/__test__/disable-2fa.test.ts
@@ -1,3 +1,64 @@
+import request from 'supertest';
+import {app} from '../../app';
+import { User } from '../../models/user';
+
 describe('disable-2fa', () => {
-    
+
+    it('should return not authorized when not logged in', async () => {
+        const response = await request(app)
+        .post('/api/users/2fa/disable')
+        .send()
+        .expect(401)
+
+        expect(response.body.errors[0].message).toEqual("Not authorized")
+    })
+
+    it('should return a bad request error when 2fa is already disabled', async () => {
+        const cookie = await global.signin();
+
+        const user = await global.user();
+
+        const response = await request(app)
+        .post('/api/users/2fa/disable')
+        .set('Cookie', cookie)
+        .send()
+        .expect(400)
+
+        expect(response.body.errors[0].message)
+        .toEqual("Two-factor authentication is not enabled")
+
+    })
+
+    it('should return not authorized when the user no longer exists', async () => {
+        const cookie = await global.signin();
+
+        await User.findOneAndDelete({email: 'test@test.com'});
+
+        const response = await request(app)
+        .post('/api/users/2fa/disable')
+        .set('Cookie', cookie)
+        .send()
+        .expect(401)
+    })
+
+    it('should correctly disabled two-factor authentication', async () => {
+        const cookie = await global.signin();
+
+        const user = await global.user();
+
+        user.set({
+            twoFactorAuthEnabled: true,
+            twoFactorAuthCode: 'test'
+        })
+
+        await user.save()
+
+        const response = await request(app)
+        .post('/api/users/2fa/disable')
+        .set('Cookie', cookie)
+        .send()
+        .expect(200)
+
+        expect(response.body.message).toEqual("Two-factor authentication disabled")
+    })
 })
--- auth/src/routes/disable-2fa.ts
+++ auth/src/routes/disable-2fa.ts
@@ -1,8 +1,5 @@
 import express, { Response, Request, NextFunction } from 'express';
-import {
-  currentUser,
-  BadRequestError,
-} from '@ramsy-dev/microservices-shop-common';
+import { currentUser, NotAuthorizedError, BadRequestError } from '@ramsy-dev/microservices-shop-common';
 import { User } from '../models/user';
 
 const router = express.Router();
@@ -13,17 +10,28 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     const { currentUser } = req;
 
+    // User is not logged in
+    if (!currentUser) {
+      throw new NotAuthorizedError()
+    }
+
     const user = await User.findById(currentUser!.id);
 
+    // User does no longer exist
+    // FIXME check permissions here
     if (!user) {
-      throw new BadRequestError('Unable to disable 2FA');
+      throw new NotAuthorizedError()
+    }
+
+    if(!user.twoFactorAuthEnabled) {
+      throw new BadRequestError('Two-factor authentication is not enabled')
     }
 
     user!.set({ twoFactorAuthEnabled: false, twoFactorAuthCode: undefined });
 
     await user!.save();
 
-    res.status(200).send({ message: 'Two-factor disabled' });
+    res.status(200).send({ message: 'Two-factor authentication disabled' });
   },
 );
 
