53bb2b7597c77ddb3aa8376507b67d3933927c28
R. de Vos <ramsy@Rs-MacBook-Pro.fritz.box>
2020-09-13 14:34:18 +0200

    Updates

--- common/src/events/cart-updated-event.ts
+++ common/src/events/cart-updated-event.ts
@@ -1,9 +1,14 @@
 import { Subjects } from './types/subjects';
 
+interface Product {
+  id: string, 
+  price: number
+}
+
 export interface CartUpdatedEvent {
   subject: Subjects.CartUpdated;
   data: {
     cartId: string;
-    products: {id: string, price: number}[]
+    products: Product[]
   };
 }
