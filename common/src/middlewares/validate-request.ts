8390fba22d72e16fe250e089431749de911f2143
Ramsy de Vos <ramsy@ramsy.dev>
2020-09-16 07:29:01 +0200

    Update validate-request
    - Rename unused variable

--- common/src/middlewares/validate-request.ts
+++ common/src/middlewares/validate-request.ts
@@ -1,10 +1,10 @@
-import {Request, Response, NextFunction} from 'express';
-import {validationResult, } from 'express-validator';
-import {RequestValidationError} from '../errors/request-validation-error';
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 export const validateRequest = (
-    req: Request, 
-    res: Response, 
+    req: Request,
+    _: Response,
     next: NextFunction
 ) => {
 
