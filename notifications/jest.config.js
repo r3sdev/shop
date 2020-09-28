d1aad71e40f2afc5ad1bc1b021db3b9ad87fd9db
Ramsy de Vos <ramsy@ramsy.dev>
2020-09-28 15:11:17 +0200

    Update test config

--- notifications/jest.config.js
+++ notifications/jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    setupFilesAfterEnv: [
-        "./src/test/setup.ts"
-    ],
     collectCoverage: true,
     coverageReporters: ["html", "lcov", "text-summary"]
 }
