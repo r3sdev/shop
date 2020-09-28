6b64e60ae14aceff7d3efc21df1af4316b0706bb
Ramsy de Vos <ramsy@ramsy.dev>
2020-09-28 14:36:19 +0200

    Update Header test
    - Fix import

--- client/components/__test__/header.spec.tsx
+++ client/components/__test__/header.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Header from '../header';
+import { Header } from '../header';
 
 describe('Header', () => {
     it('renders without crashing', () => {
