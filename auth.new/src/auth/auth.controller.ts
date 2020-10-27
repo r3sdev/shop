cfa0d29723b03f90ff738b46c64f85569587cbf8
Ramsy de Vos <ramsy@ramsy.dev>
2020-10-27 20:17:48 +0100

    Update auth

---
+++ auth.new/src/auth/auth.controller.ts
@@ -0,0 +1,53 @@
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags, ApiOkResponse, ApiCreatedResponse, ApiForbiddenResponse } from '@nestjs/swagger';
+import { User } from 'src/users';
+
+@Controller('auth')
+@ApiTags('auth')
+export class AuthController {
+ 
+    @Get('signin')
+    @ApiOperation({ summary: 'Get currently signed in user' })
+    @ApiResponse({ type: User, isArray: false, status: 200 })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })
+
+    async getSignin() {
+        return "GET signin"
+    }
+
+    @Post('signin')
+    @ApiOperation({ summary: 'Sign in user' })
+    @ApiOkResponse({ description: 'The user has successfully signed in.', type: User })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+
+    async postSignin(@Body() user: User) {
+        return "POST signin"
+    }
+
+    @Post('signup')
+    @ApiOperation({ summary: 'Sign up user' })
+    @ApiCreatedResponse({ description: 'The user has successfully signed up.', type: User })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+
+    async signup(@Body() user: User) {
+        return "POST signup"
+    }
+
+    @Delete('signout')
+    @ApiOperation({ summary: 'Sign out user' })
+    @ApiOkResponse({ description: 'The user has successfully signed out.', type: User })
+
+    async signout(@Body() user: User) {
+        return "DELETE signout"
+    }
+
+    @Get('password/new')
+    @ApiOperation({ summary: 'Get currently signed in user' })
+    @ApiOkResponse({ type: User })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+
+    async getPasswordNew() {
+        return "GET signin"
+    }
+
+}
