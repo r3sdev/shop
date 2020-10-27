52bc88576b01783e4789eaa6911b8c6b7ef4b471
Ramsy de Vos <ramsy@ramsy.dev>
2020-10-27 20:18:18 +0100

    Update main.ts

--- auth.new/src/main.ts
+++ auth.new/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app';
+import { logger } from './middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
   const configService: ConfigService = app.get(ConfigService);
 
   const scheme = configService.get('server.scheme');
@@ -12,17 +15,19 @@ async function bootstrap() {
   const port = configService.get('server.port')
 
   const options = new DocumentBuilder()
-    .setTitle('Items example')
-    .setDescription('The Items API description')
+    .setTitle('Users example')
+    .setDescription('The users API description')
     .setVersion('0.0.1')
-    .addTag('items')
+    .addTag('users')
     .setTermsOfService('http://www.ramsy.it/tos.html')
     .setContact('Ramsy IT', 'http://www.ramsy.it', 'ramsy@ramsy.it')
-    .addServer(`${scheme}://${host}:${port}`)
+    // .addServer(`${scheme}://${host}:${port}`)
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  app.use(logger)
 
   await app.listen(port);
 }
