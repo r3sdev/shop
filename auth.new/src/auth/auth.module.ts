d76ee00ae020fb7f665a718874542e4fcb4f878d
Ramsy de Vos <ramsy@ramsy.dev>
2020-10-29 19:04:31 +0100

    Update auth.module.ts
    - Import PassportModule
    - Import LocalStrategy

--- auth.new/src/auth/auth.module.ts
+++ auth.new/src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 import { ConfirmationController, ConfirmationService, ConfirmationModule } from './confirmation';
 import { PasswordController, PasswordService, PasswordModule } from './password';
 import { UsersModule } from '../users';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [ConfirmationModule, UsersModule, PasswordModule],
-  providers: [AuthService, ConfirmationService, PasswordService],
+  imports: [ConfirmationModule, UsersModule, PasswordModule, PassportModule],
+  providers: [AuthService, ConfirmationService, LocalStrategy, PasswordService],
   controllers: [AuthController, PasswordController, ConfirmationController],
 })
 export class AuthModule {}
