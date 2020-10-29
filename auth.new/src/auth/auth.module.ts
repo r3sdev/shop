9fe82416f6d5b8e26ba443102ec248b9e3778f55
Ramsy de Vos <ramsy@ramsy.dev>
2020-10-29 18:42:59 +0100

    Update auth.module.ts- Cleanup imports

--- auth.new/src/auth/auth.module.ts
+++ auth.new/src/auth/auth.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PasswordController } from './password/password.controller';
-import { ConfirmationController } from './confirmation/confirmation.controller';
-import { ConfirmationService } from './confirmation/confirmation.service';
-import { ConfirmationModule } from './confirmation/confirmation.module';
-import { PasswordService } from './password/password.service';
-import { PasswordModule } from './password/password.module';
+import { ConfirmationController, ConfirmationService, ConfirmationModule } from './confirmation';
+import { PasswordController, PasswordService, PasswordModule } from './password';
+import { UsersModule } from '../users';
 
 @Module({
+  imports: [ConfirmationModule, UsersModule, PasswordModule],
   providers: [AuthService, ConfirmationService, PasswordService],
   controllers: [AuthController, PasswordController, ConfirmationController],
-  imports: [ConfirmationModule, PasswordModule]
 })
 export class AuthModule {}
