af9e2451363e8778695022c8a8ad32c66d851441
Ramsy de Vos <ramsy@ramsy.dev>
2020-11-01 20:04:06 +0100

    Update auth.services.ts
    - Update validateUser method to use hashed passwords

--- auth.new/src/auth/auth.service.ts
+++ auth.new/src/auth/auth.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Password } from 'src/services';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) { }
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(email);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
+
+    try {
+      const isValidPassword = await Password.compare(user?.password, pass);
+
+      if (isValidPassword) {
+        const { password, ...result } = user;
+
+        return result;
+      }
+
+      return null;
     }
-    return null;
+    catch {
+      return null;
+    }
   }
 }
