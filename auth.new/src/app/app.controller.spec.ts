57493551e4ec6804324b61f6f0b36a4213b766a8
Ramsy de Vos <ramsy@ramsy.dev>
2020-11-04 18:30:56 +0100

    Move app.controller.spec.ts

--- auth.new/src/app/app.controller.spec.ts
+++ auth.new/src/app/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -14,9 +14,8 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
+
 });
