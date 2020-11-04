38b14284fd8a23b6d75c3bb52dfc252ba837471a
Ramsy de Vos <ramsy@ramsy.dev>
2020-11-04 18:32:14 +0100

    Update models

--- auth.new/src/common/models/user.model.ts
+++ auth.new/src/common/models/user.model.ts
@@ -1,15 +1,38 @@
-import { ApiProperty, ApiHideProperty,  } from "@nestjs/swagger";
+import { ApiProperty, ApiHideProperty, } from "@nestjs/swagger";
+import { Expose } from 'class-transformer';
 
 export class User {
-    @ApiHideProperty() 
+    @ApiHideProperty()
     _id: string;
 
-    @ApiProperty({example: "user@tld.com", description: "The user's email address"}) 
+    @ApiProperty({ example: "user@tld.com", description: "The user's email address" })
     email: string;
 
-    @ApiProperty({description: "The user's full name"}) 
-    fullName: string;
-    
-    @ApiHideProperty() 
+    @ApiProperty({ example: "John", description: "The user's first name" })
+    firstName: string;
+
+    @ApiProperty({ example: "Doe", description: "The user's last name" })
+    lastName: string;
+
+    @ApiHideProperty()
     password: string;
+
+    @Expose()
+    @ApiProperty({ example: "John Doe", description: "The user's full name", readOnly: true })
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    constructor(partial: Partial<User>) {
+        Object.assign(this, partial);
+    }
+
+    toJSON() {
+        const {_id, ...user} = this;
+
+        return {
+            id: _id,
+            ...user
+        }
+    }
 }
