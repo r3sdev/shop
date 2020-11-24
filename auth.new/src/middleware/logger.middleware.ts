8c877484b61498210519851f485276b2e57e7377
Ramsy de Vos <ramsy@ramsy.dev>
2020-11-24 11:06:21 +0100

    refactor(logger): rename response variable

--- auth.new/src/middleware/logger.middleware.ts
+++ auth.new/src/middleware/logger.middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-export function logger(req: Request, res: Response, next: Function) {
+export function logger(req: Request, _: Response, next: Function) {
   console.debug(
     `[${req.ip}] `,
     new Date().toUTCString(), 
