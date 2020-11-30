f72400e1d582089d00d76129f321b6c6d9b03b71
Ramsy de Vos <ramsy@ramsy.dev>
2020-11-30 15:43:45 +0100

    feat(app): add root index

--- auth.new/src/app/app.controller.ts
+++ auth.new/src/app/app.controller.ts
@@ -1,6 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
     // TODO: Implement security: see https://docs.nestjs.com/techniques/security
+    @Get()
+    root(): string {
+      return 'Hello World!';
+    }
 }
